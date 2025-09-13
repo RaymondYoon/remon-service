d2e714a0fdffff1b32159653d981883af8de34da
Raymond Yoon <yoonjunsang@naver.com>
2025-09-13 19:32:52 +0900

    feat: User entity 추가

---
+++ src/main/java/com/remon/User/entity/User.java
@@ -0,0 +1,35 @@
+package com.remon.User.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
+    @Column(nullable = false)
+    private String provider;
+
+    private String providerId;
+
+    private String nickname;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private boolean emailVerified = false;
+
+}
