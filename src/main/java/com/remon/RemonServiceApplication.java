a7ec158c842ebff2cef8b7cd96c3c9d9e40c262f
Raymond Yoon <yoonjunsang@naver.com>
2025-09-13 18:35:26 +0900

    Init Spring Bott project

---
+++ src/main/java/com/remon/RemonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.remon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemonServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remon/RemonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
