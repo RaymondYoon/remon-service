eb004d96f873da33727fc1d063bcf6687009a87b
Raymond Yoon <yoonjunsang@naver.com>
2025-09-16 00:23:09 +0900

    feat:이메일 기준 회원 조회

--- src/main/java/com/remon/User/service/UserService.java
+++ src/main/java/com/remon/User/service/UserService.java
@@ -35,4 +35,8 @@ public class UserService {
 
         return userRepository.save(user);
     }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
