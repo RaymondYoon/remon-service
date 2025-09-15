e41640a06c14ba2d60839113e2ab2e33b8821c4b
Raymond Yoon <yoonjunsang@naver.com>
2025-09-15 23:56:59 +0900

    UserRepository interface 추가

--- src/main/java/com/remon/User/repository/UserRepository.java
+++ src/main/java/com/remon/User/repository/UserRepository.java
@@ -1,2 +1,11 @@
-package com.remon.User.repository;public class UserRepository {
+package com.remon.User.repository;
+
+import com.remon.User.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
