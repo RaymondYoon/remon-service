8beda3992c37aea601623352b6bdbdd5a42f5eea
Raymond Yoon <yoonjunsang@naver.com>
2025-10-10 17:25:47 +0900

    feat: Add Book entity for eBook service

---
+++ src/main/java/com/remon/book/entity/Book.java
@@ -0,0 +1,29 @@
+package com.remon.book.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private String isbn;
+    private LocalDate publishedDate;
+    private double price;
+    private String description;
+}
