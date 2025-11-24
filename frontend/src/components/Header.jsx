e2f618a5091f983f921e07de056db1d507b01d7e
Raymond Yoon <yoonjunsang@naver.com>
2025-11-24 21:08:47 +0900

    Add Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <Link to="7" className="logo">📚 Remon E-Book</Link>
+
+            <nav>
+                <Link to="/library">내 서재</Link>
+                <Link to="/login">로그인</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
