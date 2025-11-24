1736a8f9f8d116968fd9a848b0c5b5356e018bc3
Raymond Yoon <yoonjunsang@naver.com>
2025-11-24 21:09:01 +0900

    Add Login page UI

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
+function Login() {
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+    
+
+     if (!email || !password) {
+         setError("이메일과 비밀번호를 입력해주세요.")
+         return;
+     }
+ 
+     navigate("/");
+ };
+ 
+ return (
+   <div className="login-container">
+     <h2>로그인</h2>
+ 
+     <form className="login-form" onSubmit={handleLogin}>
+       <input
+         type="email"
+         placeholder="이메일 입력"
+         value={email}
+         onChange={(e) => setEmail(e.target.value)}
+         />
+ 
+         <input
+         type="password"
+         placeholder="비밀번호 입력"
+         value={password}
+         onChange={(e) => setPassword(e.target.value)}
+         />
+ 
+         {error && <p className="login-error">{error}</p>}
+ 
+         <button type="submit" className="login-btn">
+             로그인
+         </button>
+     </form>
+ 
+     <p className="login-helper">
+       계정이 없나요? <span className="signup-link">회원가입</span>
+     </p>
+ </div>
+ );
+}
+
+export default Login;
