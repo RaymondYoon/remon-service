4ad33ebc7b65e0e8f8ec3132a3f7465bca039898
Raymond Yoon <yoonjunsang@naver.com>
2026-02-04 19:40:44 +0900

    fix : BCryptPasswordEncoder사용하여 비밀번호 암호화

--- backend/src/main/java/com/remon/user/service/UserService.java
+++ backend/src/main/java/com/remon/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.remon.user.service;
 import com.remon.user.entity.Role;
 import com.remon.user.entity.User;
 import com.remon.user.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +14,25 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    public User login (String email, String password) {
+        Optional<User> userOptional = userRepository.findByEmail(email);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (bCryptPasswordEncoder.matches(password, user.getPassword())) {
+                return user;
+            } else {
+                throw new IllegalStateException("잘못된 비밀번호입니다.");
+            }
+        } else {
+            throw new IllegalStateException("존재하지 않는 이메일입니다.");
+        }
     }
 
     public User registerUser(String email, String password, String provider, String providerId, String nickname){
@@ -23,10 +40,10 @@ public class UserService {
         if (existingUser.isPresent()) {
             throw new IllegalStateException("이미 가입된 이메일 입니다.");
         }
-
+        String encodedPassword = bCryptPasswordEncoder.encode(password);
         User user = User.builder()
                 .email(email)
-                .password(password)
+                .password(encodedPassword)
                 .provider(provider)
                 .providerId(providerId)
                 .nickname(nickname)
